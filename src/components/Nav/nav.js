9eb4acb696a664916cfa2447a8b74edd124fa973
Sh4ke96 <Sh4ke96@gmail.com>
2021-09-04 23:35:24 +0200

    add nav menu for desktop

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -1,10 +1,32 @@
 import React from "react";
-import { Container } from "./nav.styled";
+import { Container, DesktopMenu } from "./nav.styled";
 import Button from "../Button/button";
 
 function Nav() {
   return (
     <Container>
+      <DesktopMenu>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Service</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Features</a>
+          </li>
+          <li>
+            <a href="/">Team</a>
+          </li>
+          <li>
+            <a href="/">Pricing</a>
+          </li>
+        </ul>
+      </DesktopMenu>
       <Button>Free sign up</Button>
       <i class="fas fa-bars Nav-icon"></i>
     </Container>
--- src/components/Nav/nav.styled.js
+++ src/components/Nav/nav.styled.js
@@ -7,8 +7,52 @@ export const Container = styled.nav`
     color: #73299a;
     margin-left: 15px;
     font-size: 30px;
+    @media screen and (min-width: 1000px) {
+      display: none;
+    }
   }
   &:hover {
     cursor: pointer;
+  }
+`;
+
+export const DesktopMenu = styled.div`
+  display: none;
+  @media screen and (min-width: 1000px) {
+    display: block;
+  }
+  ul {
+    display: flex;
+    list-style: none;
+    li {
+      display: flex;
+      position: relative;
+      margin: 0 15px;
+      font-weight: 600;
+      a {
+        display: inline-block;
+        text-decoration: none;
+        color: #212529;
+        transition: 0.3s all ease-in-out;
+      }
+      a:hover {
+        color: #73299a;
+      }
+    }
+    li::after {
+      content: "";
+      position: absolute;
+      bottom: -10px;
+      width: 0px;
+      height: 3px;
+      margin: 5px 0 0;
+      transition-duration: 0.75s;
+      opacity: 0;
+      background-color: #73299a;
+    }
+    li:hover::after {
+      width: 100%;
+      opacity: 1;
+    }
   }
 `;
