9e2b06f5ce8d4fdfb70a08eab9df913daf49ad87
Sh4ke96 <Sh4ke96@gmail.com>
2021-09-04 20:46:35 +0200

    clean up project, add basic reset css, start to style Header component

---
+++ src/components/Header/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./header.styled";
+
+function Header() {
+  return (
+    <Container>
+      <img src="images/logo.png" alt="sassio-word-logo" />
+    </Container>
+  );
+}
+
+export default Header;
---
+++ src/components/Header/header.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 15px;
+  box-shadow: 0px 11px 68px 0px rgba(0, 0, 0, 0.14);
+`;
