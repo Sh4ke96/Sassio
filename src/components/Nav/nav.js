041f8c9e2547a14374902446fc30f5a0bd654d7e
Sh4ke96 <Sh4ke96@gmail.com>
2021-09-06 20:11:13 +0200

    add mobile menu

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Container, DesktopMenu } from "./nav.styled";
+import { useState } from "react";
+import { Container, DesktopMenu, MobileMenu } from "./nav.styled";
 import Button from "../Button/button";
 
 function Nav() {
+  const [setMobileMenuOpen, MobileMenuIsOpen] = useState(false);
+  function openHandler() {
+    MobileMenuIsOpen(true);
+  }
+  function closeHandler() {
+    MobileMenuIsOpen(false);
+  }
   return (
     <Container>
       <DesktopMenu>
@@ -28,7 +36,36 @@ function Nav() {
         </ul>
       </DesktopMenu>
       <Button>Free sign up</Button>
-      <i class="fas fa-bars Nav-icon"></i>
+      <i className="fas fa-bars Nav-icon" onClick={openHandler}></i>
+      {setMobileMenuOpen ? (
+        <MobileMenu>
+          <i
+            className="fas fa-times mobile-close-icon"
+            onClick={closeHandler}
+          ></i>
+          <img src="images/logo.png" alt="sassio-word-logo" />
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Service</a>
+            </li>
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">Features</a>
+            </li>
+            <li>
+              <a href="/">Team</a>
+            </li>
+            <li>
+              <a href="/">Pricing</a>
+            </li>
+          </ul>
+        </MobileMenu>
+      ) : null}
     </Container>
   );
 }
--- src/components/Nav/nav.styled.js
+++ src/components/Nav/nav.styled.js
@@ -11,7 +11,7 @@ export const Container = styled.nav`
       display: none;
     }
   }
-  &:hover {
+  .Nav-icon:hover {
     cursor: pointer;
   }
 `;
@@ -24,10 +24,71 @@ export const DesktopMenu = styled.div`
   ul {
     display: flex;
     list-style: none;
+    margin-right: 15px;
     li {
       display: flex;
       position: relative;
       margin: 0 15px;
+      font-weight: 600;
+      a {
+        display: inline-block;
+        text-decoration: none;
+        color: #212529;
+        transition: 0.3s all ease-in-out;
+      }
+      a:hover {
+        color: #73299a;
+      }
+    }
+    li::after {
+      content: "";
+      position: absolute;
+      bottom: -10px;
+      width: 0px;
+      height: 3px;
+      margin: 5px 0 0;
+      transition-duration: 0.75s;
+      opacity: 0;
+      background-color: #73299a;
+    }
+    li:hover::after {
+      width: 100%;
+      opacity: 1;
+    }
+  }
+`;
+
+export const MobileMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 280px;
+  height: 100vh;
+  overflow-y: scroll;
+  background-color: #fff;
+  padding: 40px 0px;
+  box-shadow: 0px 3px 5px rgba(100, 100, 100, 0.19);
+  .mobile-close-icon {
+    font-size: 30px;
+    margin-bottom: 30px;
+  }
+  .mobile-close-icon:hover {
+    cursor: pointer;
+    color: #73299a;
+  }
+  ul {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    list-style: none;
+    margin-top: 30px;
+    li {
+      display: flex;
+      position: relative;
+      margin: 15px 0;
       font-weight: 600;
       a {
         display: inline-block;
