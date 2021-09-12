2211cfa88019e26038c2121e134949c5e9c49f4e
Sh4ke96 <Sh4ke96@gmail.com>
2021-09-12 23:16:56 +0200

    add RWD for heading component

--- src/components/Heading/heading.js
+++ src/components/Heading/heading.js
@@ -5,30 +5,32 @@ import Button from "../Button/button";
 function Heading() {
   return (
     <Container>
-      <span className="heading-title-tag">Creative SAAS webiste</span>
-      <h1 className="heading-title">
-        Best website for <span className="heading-title-span">SAAS</span>{" "}
-        business
-      </h1>
-      <p className="heading-text">
-        Even in the most uncertain times, Help Scout keeps you connected with
-        customers.
-      </p>
-      <div className="heading-buttons">
-        <div className="heading-buttons-container">
-          <a href="/" className="heading-link">
-            <Button className="heading-button">
-              <i className="fas fa-cloud-download-alt heading-icon"></i>Try it
-              free
-            </Button>
-          </a>
-          <a href="/" className="heading-link">
-            <Button className="heading-button">
-              <i className="fas fa-clipboard heading-icon"></i>Get a demo
-            </Button>
-          </a>
+      <div className="heading-wrapper">
+        <span className="heading-title-tag">Creative SAAS webiste</span>
+        <h1 className="heading-title">
+          Best website for <span className="heading-title-span">SAAS</span>{" "}
+          business
+        </h1>
+        <p className="heading-text">
+          Even in the most uncertain times, Help Scout keeps you connected with
+          customers.
+        </p>
+        <div className="heading-buttons">
+          <div className="heading-buttons-container">
+            <a href="/" className="heading-link">
+              <Button className="heading-button">
+                <i className="fas fa-cloud-download-alt heading-icon"></i>Try it
+                free
+              </Button>
+            </a>
+            <a href="/" className="heading-link">
+              <Button className="heading-button">
+                <i className="fas fa-clipboard heading-icon"></i>Get a demo
+              </Button>
+            </a>
+          </div>
+          <span className="heading-buttons-span">No credit card required*</span>
         </div>
-        <span className="heading-buttons-span">No credit card required*</span>
       </div>
       <div className="heading-mockup">
         <img
--- src/components/Heading/heading.styled.js
+++ src/components/Heading/heading.styled.js
@@ -5,51 +5,89 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 100px 15px 15px 15px;
-  .heading-title-tag {
-    color: #951fb3;
-    font-size: 18px;
-    font-weight: 500;
-    padding: 3px 30px;
-    border-radius: 8px;
-    display: inline-block;
-    background-color: #ede0f4;
+  @media screen and (min-width: 1000px) {
+    flex-direction: row;
+    max-width: 1200px;
+    align-items: flex-start;
+    margin: 0 auto;
+    padding: 150px 15px 15px 15px;
   }
-  .heading-title {
-    margin: 15px 0;
-    font-size: 36px;
-    text-align: center;
-    .heading-title-span {
-    }
-  }
-  .heading-text {
-    text-align: center;
-    margin-bottom: 15px;
-  }
-  .heading-buttons {
+  .heading-wrapper {
     display: flex;
     flex-direction: column;
     align-items: center;
-    .heading-buttons-container {
+    max-width: 450px;
+    @media screen and (min-width: 1000px) {
+      align-items: flex-start;
+    }
+    .heading-title-tag {
+      color: #951fb3;
+      font-size: 18px;
+      font-weight: 500;
+      padding: 3px 30px;
+      border-radius: 8px;
+      display: inline-block;
+      background-color: #ede0f4;
+    }
+    .heading-title {
+      margin: 15px 0;
+      font-size: 36px;
+      text-align: center;
+      @media screen and (min-width: 1000px) {
+        text-align: left;
+        font-size: 45px;
+      }
+      .heading-title-span {
+        color: #951fb3;
+      }
+    }
+    .heading-text {
+      text-align: center;
+      margin-bottom: 30px;
+      @media screen and (min-width: 1000px) {
+        text-align: left;
+      }
+    }
+    .heading-buttons {
       display: flex;
-      .heading-link {
-        text-decoration: none;
-        .heading-button {
-          margin: 0 10px;
-          .heading-icon {
-            margin-right: 5px;
+      flex-direction: column;
+      align-items: center;
+      @media screen and (min-width: 1000px) {
+        align-items: flex-start;
+      }
+      .heading-buttons-container {
+        display: flex;
+        justify-content: space-between;
+        .heading-link {
+          display: block;
+          text-decoration: none;
+          .heading-button {
+            height: 50px;
+            width: 175px;
+            font-size: 18px;
+            margin-right: 15px;
+            .heading-icon {
+              margin-right: 10px;
+            }
           }
         }
       }
-    }
-    .heading-buttons-span {
-      color: #6a6a6a;
-      display: block;
-      font-size: 14px;
-      margin-top: 5px;
+      .heading-buttons-span {
+        color: #6a6a6a;
+        display: block;
+        font-size: 14px;
+        margin-top: 10px;
+        @media screen and (min-width: 1000px) {
+          margin-top: 20px;
+        }
+      }
     }
   }
   .heading-mockup {
     margin: 30px 0;
+    @media screen and (min-width: 1000px) {
+      margin: 0;
+    }
     .heading-mockup-img {
       max-width: 100%;
       height: auto;
