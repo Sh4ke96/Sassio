0b9ac04ab857634cb81463d101573d5c4dc60a47
Sh4ke96 <Sh4ke96@gmail.com>
2021-09-18 20:41:22 +0200

    add RWD to footer component

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -5,105 +5,96 @@ import Button from "../Button/button";
 function Footer() {
   return (
     <Container>
-      <div className="footer-newsletter">
-        <h1 className="newsletter-title">Subscribe now!</h1>
-        <span className="newsletter-text">Get the latest update.</span>
-        <div className="newsletter-input-container">
-          <input
-            placeholder="Enter your email address"
-            className="newsletter-input"
-          />
-          <Button className="newsletter-button">Subscribe</Button>
-        </div>
-      </div>
-      <div className="footer-main">
-        <div className="footer-main-info">
-          <img src="/images/logo-light.png" alt="" />
-          <div className="footer-main-about">
-            <div className="footer-main-about-text-container">
-              <span className="footer-main-about-text">
-                30 Commercial Road Fratton
-              </span>
-              <span className="footer-main-about-text">
-                PORTSMOUTH Hampshire PO1 1AA
-              </span>
-              <span className="footer-main-about-text">UNITED KINGDOM</span>
-            </div>
-            <div className="footer-main-open">
-              <span className="footer-main-open-hours">Open Hours:</span>
-              <span className="footer-main-open-hours">
-                Mon - Sat: 8am - 5pm
-              </span>
+      <div className="footer-wrapper">
+        <div className="footer-main">
+          <div className="footer-main-info">
+            <img src="/images/logo-light.png" alt="" />
+            <div className="footer-main-about">
+              <div className="footer-main-about-text-container">
+                <span className="footer-main-about-text">
+                  30 Commercial Road Fratton
+                </span>
+                <span className="footer-main-about-text">
+                  PORTSMOUTH Hampshire PO1 1AA
+                </span>
+                <span className="footer-main-about-text">UNITED KINGDOM</span>
+              </div>
+              <div className="footer-main-open">
+                <span className="footer-main-open-hours">Open Hours:</span>
+                <span className="footer-main-open-hours">
+                  Mon - Sat: 8am - 5pm
+                </span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="footer-links">
-        <h2 className="footer-links-title">Links</h2>
-        <div className="footer-links-container">
-          <ul className="footer-links-ul">
-            <a href="/" className="footer-links-link">
-              <li className="footer-links-item">
-                <i className="fas fa-chevron-right"></i>Home
-              </li>
-            </a>
-            <a href="/" className="footer-links-link">
-              <li className="footer-links-item">
-                <i className="fas fa-chevron-right"></i>FAQ
-              </li>
-            </a>
-            <a href="/" className="footer-links-link">
-              <li className="footer-links-item">
-                <i className="fas fa-chevron-right"></i>Gallery
-              </li>
-            </a>
-            <a href="/" className="footer-links-link">
-              <li className="footer-links-item">
-                <i className="fas fa-chevron-right"></i>Contact
-              </li>
-            </a>
-            <a href="/" className="footer-links-link">
-              <li className="footer-links-item">
-                <i className="fas fa-chevron-right"></i>News
-              </li>
-            </a>
-          </ul>
-          <ul className="footer-links-ul">
-            <a href="/" className="footer-links-link">
-              <li className="footer-links-item">
-                <i className="fas fa-chevron-right"></i>Team
-              </li>
-            </a>
-            <a href="/" className="footer-links-link">
-              <li className="footer-links-item">
-                <i className="fas fa-chevron-right"></i>Services
-              </li>
-            </a>
-            <a href="/" className="footer-links-link">
-              <li className="footer-links-item">
-                <i className="fas fa-chevron-right"></i>About us
-              </li>
-            </a>
-            <a href="/" className="footer-links-link">
-              <li className="footer-links-item">
-                <i className="fas fa-chevron-right"></i>Testimonials
-              </li>
-            </a>
-            <a href="/" className="footer-links-link">
-              <li className="footer-links-item">
-                <i className="fas fa-chevron-right"></i>Portfolio
-              </li>
-            </a>
-          </ul>
+        <div className="footer-links">
+          <h2 className="footer-links-title">Links</h2>
+          <div className="footer-links-container">
+            <ul className="footer-links-ul">
+              <a href="/" className="footer-links-link">
+                <li className="footer-links-item">
+                  <i className="fas fa-chevron-right"></i>Home
+                </li>
+              </a>
+              <a href="/" className="footer-links-link">
+                <li className="footer-links-item">
+                  <i className="fas fa-chevron-right"></i>FAQ
+                </li>
+              </a>
+              <a href="/" className="footer-links-link">
+                <li className="footer-links-item">
+                  <i className="fas fa-chevron-right"></i>Gallery
+                </li>
+              </a>
+              <a href="/" className="footer-links-link">
+                <li className="footer-links-item">
+                  <i className="fas fa-chevron-right"></i>Contact
+                </li>
+              </a>
+              <a href="/" className="footer-links-link">
+                <li className="footer-links-item">
+                  <i className="fas fa-chevron-right"></i>News
+                </li>
+              </a>
+            </ul>
+            <ul className="footer-links-ul">
+              <a href="/" className="footer-links-link">
+                <li className="footer-links-item">
+                  <i className="fas fa-chevron-right"></i>Team
+                </li>
+              </a>
+              <a href="/" className="footer-links-link">
+                <li className="footer-links-item">
+                  <i className="fas fa-chevron-right"></i>Services
+                </li>
+              </a>
+              <a href="/" className="footer-links-link">
+                <li className="footer-links-item">
+                  <i className="fas fa-chevron-right"></i>About us
+                </li>
+              </a>
+              <a href="/" className="footer-links-link">
+                <li className="footer-links-item">
+                  <i className="fas fa-chevron-right"></i>Testimonials
+                </li>
+              </a>
+              <a href="/" className="footer-links-link">
+                <li className="footer-links-item">
+                  <i className="fas fa-chevron-right"></i>Portfolio
+                </li>
+              </a>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div className="footer-socials">
-        <h2 className="footer-socials-title">Socials</h2>
-        <div className="footer-socials-container">
-          <i className="footer-socials-icon fab fa-facebook"></i>
-          <i className="footer-socials-icon fab fa-instagram"></i>
-          <i className="footer-socials-icon fab fa-twitter-square"></i>
-          <i className="footer-socials-icon fab fa-behance-square"></i>
+        <div className="footer-socials">
+          <h2 className="footer-socials-title">Socials</h2>
+          <div className="footer-socials-container">
+            <i className="footer-socials-icon fab fa-facebook"></i>
+            <i className="footer-socials-icon fab fa-instagram"></i>
+            <i className="footer-socials-icon fab fa-twitter-square"></i>
+            <i className="footer-socials-icon fab fa-behance-square"></i>
+          </div>
         </div>
       </div>
     </Container>
--- src/components/Footer/footer.styled.js
+++ src/components/Footer/footer.styled.js
@@ -3,125 +3,116 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   padding: 30px 15px 30px 15px;
   background-color: #1b0234;
   overflow: hidden;
-  .footer-newsletter {
+  .footer-wrapper {
     display: flex;
     flex-direction: column;
-    align-items: center;
-    border-radius: 10px;
-    padding: 30px;
-    background-color: #6c2b95;
-    box-shadow: 0px 0px 16px 0px rgba(27, 2, 52, 0.34);
-    .newsletter-title {
-      line-height: 1;
-      color: white;
+    max-width: 1200px;
+    @media screen and (min-width: 1000px) {
+      flex-direction: row;
+      justify-content: center;
     }
-    .newsletter-text {
-      color: white;
-      font-size: 14px;
-    }
-    .newsletter-input-container {
+    .footer-main {
       display: flex;
       flex-direction: column;
-      align-items: center;
-      margin-top: 30px;
-      .newsletter-input {
-        height: 60px;
-        color: #6c2b95;
-        border: none;
-        max-width: 490px;
-        padding-left: 30px;
-        border-radius: 5px;
+      align-items: flex-start;
+      max-width: 1200px;
+      padding: 30px 15px 30px 15px;
+      @media screen and (min-width: 1000px) {
+        padding: 15px;
       }
-      .newsletter-input:focus {
-        outline: none;
-      }
-      .newsletter-button {
-        background-color: #f104f5;
-        margin-top: 15px;
-      }
-    }
-  }
-  .footer-main {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    padding: 30px 15px 30px 15px;
-    .footer-main-info {
-      color: white;
-      .img {
-        width: 100%;
-        height: auto;
-      }
-      .footer-main-about-text-container {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        margin-top: 15px;
-        .footer-main-about-text {
-          max-width: 235px;
-          line-height: 1.714;
-          font-size: 14px;
+      .footer-main-info {
+        color: white;
+        .img {
+          width: 100%;
+          height: auto;
         }
-      }
-      .footer-main-open {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        margin-top: 15px;
-        .footer-main-open-hours {
-          max-width: 235px;
-          line-height: 1.714;
-          font-size: 14px;
+        .footer-main-about-text-container {
+          display: flex;
+          flex-direction: column;
+          align-items: flex-start;
+          margin-top: 15px;
+          .footer-main-about-text {
+            max-width: 235px;
+            line-height: 1.714;
+            font-size: 14px;
+          }
+        }
+        .footer-main-open {
+          display: flex;
+          flex-direction: column;
+          align-items: flex-start;
+          margin-top: 15px;
+          .footer-main-open-hours {
+            max-width: 235px;
+            line-height: 1.714;
+            font-size: 14px;
+          }
         }
       }
     }
-  }
-  .footer-links {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    padding: 0 15px 30px 15px;
-    color: white;
-    .footer-links-title {
-      font-size: 25px;
-    }
-    .footer-links-container {
-      margin-top: 15px;
+    .footer-links {
       display: flex;
-      .footer-links-ul {
-        margin-right: 50px;
-        .footer-links-link {
-          text-decoration: none;
-          color: white;
-          .footer-links-item {
-            padding: 5px 0;
-            list-style: none;
-            .fa-chevron-right {
-              font-size: 14px;
-              margin-right: 8px;
+      flex-direction: column;
+      align-items: flex-start;
+      padding: 0 15px 30px 15px;
+      max-width: 1200px;
+      color: white;
+      @media screen and (min-width: 1000px) {
+        padding: 15px;
+      }
+      .footer-links-title {
+        font-size: 25px;
+      }
+      .footer-links-container {
+        margin-top: 15px;
+        display: flex;
+        .footer-links-ul {
+          margin-right: 50px;
+          .footer-links-link {
+            text-decoration: none;
+            color: white;
+            .footer-links-item {
+              padding: 5px 0;
+              list-style: none;
+              .fa-chevron-right {
+                font-size: 14px;
+                margin-right: 8px;
+              }
             }
           }
         }
       }
     }
-  }
-  .footer-socials {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    padding: 0 15px 0 15px;
-    color: white;
-    .footer-socials-title {
-      font-size: 25px;
-    }
-    .footer-socials-container {
-      margin-top: 15px;
-      .footer-socials-icon {
-        margin-right: 20px;
+    .footer-socials {
+      max-width: 1200px;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      padding: 0 15px 0 15px;
+      color: white;
+      @media screen and (min-width: 1000px) {
+        padding: 15px;
+      }
+      .footer-socials-title {
         font-size: 25px;
+      }
+      .footer-socials-container {
+        margin-top: 15px;
+        @media screen and (min-width: 1000px) {
+          display: flex;
+          flex-direction: column;
+        }
+        .footer-socials-icon {
+          margin-right: 20px;
+          font-size: 25px;
+          @media screen and (min-width: 1000px) {
+            margin-bottom: 20px;
+          }
+        }
       }
     }
   }
